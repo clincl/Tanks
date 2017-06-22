4172f7f1609f0c63509288c91e38b893018ee514
mcham1 <mcham1@users.noreply.github.com>
2017-06-22 09:52:16 -0400

    Create utility.py

---
+++ utility.py
@@ -0,0 +1,10 @@
+import pygame
+
+def loadImage(image):
+    filename = os.path.join
+    image = pygame.image.load(filename).convert()
+    return image, image.get_rect()
+    
+def Terrain():
+
+def 
