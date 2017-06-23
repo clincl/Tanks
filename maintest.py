a229d29c9ec2f9d19a9eecf76471c69fb74f5dbf
clincl <clincl@users.noreply.github.com>
2017-06-23 11:09:32 -0400

    Update maintest.py

--- maintest.py
+++ maintest.py
@@ -9,64 +9,64 @@ def test():
 	Returns: None
 	'''
 	#This tests object creation
-	tank = Tank()
-	print(type(tank))
-	
+	tank1 = tank.Tank()
+	print(type(tank1))
+
 	#This tests the class to ensure that the stats are assigned properly
 
 	#This tests proper movement in the positive x direction
-	print("The old x-pos of the tank is ", tank.tankRect.x())
+	print("The old x-pos of the tank is ", tank1.tankRect.x())
 	tank.move(pygame.K_RIGHT)
-	print("The new x-pos of the tank is ", tank.tankRect.x())	
-	
+	print("The new x-pos of the tank is ", tank1.tankRect.x())
+
 	#This tests proper movement in the negative x direction
-	print("The old x-pos of the tank is ", tank.tankRect.x())
+	print("The old x-pos of the tank is ", tank1.tankRect.x())
 	tank.move(pygame.K_LEFT)
-	print("The new x-pos of the tank is ", tank.tankRect.x())	
-	
+	print("The new x-pos of the tank is ", tank1.tankRect.x())
+
 	#This tests the upward angular movement of the shootything
-	print("The old x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
+	print("The old x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
 	tank.angle(pygame.K_UP)
-	print("The new x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
-	
+	print("The new x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
+
 	#This tests the downward angular movement of the shootything
-	print("The old x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
+	print("The old x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
 	tank.angle(pygame.K_DOWN)
-	print("The new x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
-	
+	print("The new x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
+
 	#This tests to see if the tank will move if non-essential keys are pressed
-	print("The old x-pos of the tank is ", tank.tankRect.x())
+	print("The old x-pos of the tank is ", tank1.tankRect.x())
 	tank.move(pygame.K_V)
-	print("The new x-pos of the tank is ", tank.tankRect.x())
+	print("The new x-pos of the tank is ", tank1.tankRect.x())
 
-	print("The old x-pos of the tank is ", tank.tankRect.x())
+	print("The old x-pos of the tank is ", tank1.tankRect.x())
 	tank.move(pygame.K_4)
-	print("The new x-pos of the tank is ", tank.tankRect.x())
+	print("The new x-pos of the tank is ", tank1.tankRect.x())
 
-	print("The old x-pos of the tank is ", tank.tankRect.x())
+	print("The old x-pos of the tank is ", tank1.tankRect.x())
 	tank.move(pygame.K_BACKSLASH)
-	print("The new x-pos of the tank is ", tank.tankRect.x())
+	print("The new x-pos of the tank is ", tank1.tankRect.x())
 
 	#This tests to see if the shootything moves if non-essential keys are pressed
-	print("The old x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
+	print("The old x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
 	tank.angle(pygame.K_V)
-	print("The new x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
-	
-	print("The old x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
+	print("The new x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
+
+	print("The old x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
 	tank.angle(pygame.K_4)
-	print("The new x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
-	
-	print("The old x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
+	print("The new x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
+
+	print("The old x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
 	tank.angle(pygame.K_BACKSLASH)
-	print("The new x-pos and y-pos of the tank is:", tank.shootyThingRect.x(), "and", tank.shootyThingRect.y())
-	
+	print("The new x-pos and y-pos of the tank is:", tank1.shootyThingRect.x(), "and", tank1.shootyThingRect.y())
+
 	#This tests to see if the tank shoots properly
 	tank.shoot(pygame.K_SPACE)
 
 	#This tests to see if the tank takes damage when hit
-	
+
 	#This tests
-	
+
 	#More power== key held down
 	#Transform - rotating objects
 
