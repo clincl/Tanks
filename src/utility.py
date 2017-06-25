ebd55491016c55c40a6ca329f3d2763d7b0582b6
mcham1 <mcham1@users.noreply.github.com>
2017-06-25 19:46:56 -0400

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -1,13 +1,13 @@
-
 ####MAKE SURE TO CHANGE FOLDERNAME
 import pygame
 import os
 
-GROUND = 5
+GROUND = 250
 BLACK = (0,0,0)
 WHITE = (255, 255, 255)
 PURPLE = (216, 191, 216)
 YELLOW = (255, 255, 51)
+INVALID_INPUTS = [pygame.K_4, pygame.K_h, pygame.K_i, pygame.K_SEMICOLON, pygame.K_MINUS, pygame.K_LEFTBRACKET, pygame.K_QUESTION, pygame.K_HASH, pygame.K_v, pygame.K_KP4]
 
 def loadImage(image):
     filename = os.path.join("images", image)
@@ -16,4 +16,16 @@ def loadImage(image):
     
 #def Terrain():
 
-#def 
+def testMove(functionality, Window, Tank):
+	functionality
+	Window.screen.blit(Tank.tankImage, (Tank.tankRect.x,Tank.tankRect.y))
+	Window.screen.blit(Tank.shootyThingImage, (Tank.shootyThingRect.x,Tank.shootyThingRect.y))		
+	pygame.display.update()
+
+def testAngle(functionality, init_angle, Window, Tank):	
+	angle = init_angle	
+	angle = functionality
+	Window.screen.blit(Tank.tankImage, (Tank.tankRect.x,Tank.tankRect.y))
+	Window.screen.blit(Tank.shootyThingImage, (Tank.shootyThingRect.x,Tank.shootyThingRect.y))
+	pygame.display.update()
+	return angle
