980da5eeec41ceb3aa034d5992678ee3d7084ac9
mcham1 <mcham1@users.noreply.github.com>
2017-06-25 19:45:27 -0400

    Update bullet.py

--- src/bullet.py
+++ src/bullet.py
@@ -3,10 +3,11 @@ import utility
 
 class Bullet(pygame.sprite.Sprite):
     
-    def __init__(self, location, image):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.bulletX, self.bulletY = location
         self.bulletImage, self.bulletRect = utility.loadImage("myBullet.jpg")
+        self.bulletRect.x = x + 75
+        self.bulletRect.y = y
         self.bulletXSpeed = 5
         self.bulletYSpeed = 5
         self.bulletMaxY = 150
