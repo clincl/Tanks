63ee81ddc224d182c1af4c32e85d87f73f319b4e
mcham1 <mcham1@users.noreply.github.com>
2017-06-25 15:18:49 -0400

    Update utility.py

--- utility.py
+++ utility.py
@@ -5,9 +5,11 @@ import os
 GROUND = 5
 BLACK = (0,0,0)
 WHITE = (255, 255, 255)
+PURPLE = (216, 191, 216)
+YELLOW = (255, 255, 51)
 
 def loadImage(image):
-    filename = os.path.join("foldername", image)
+    filename = os.path.join("images", image)
     image = pygame.image.load(filename).convert()
     return image, image.get_rect()
     
