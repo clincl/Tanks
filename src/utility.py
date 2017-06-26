9348cb8346fdfa2ca6496205b12efafd99aa0772
clincl <clincl@users.noreply.github.com>
2017-06-25 22:53:38 -0400

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -1,5 +1,5 @@
 ####MAKE SURE TO CHANGE FOLDERNAME
-import pygame
+import pygame as pg
 import os
 
 TITLE = 'Tanks'
@@ -8,11 +8,11 @@ BLACK = (0,0,0)
 WHITE = (255, 255, 255)
 PURPLE = (216, 191, 216)
 YELLOW = (255, 255, 51)
-INVALID_INPUTS = [pygame.K_4, pygame.K_h, pygame.K_i, pygame.K_SEMICOLON, pygame.K_MINUS, pygame.K_LEFTBRACKET, pygame.K_QUESTION, pygame.K_HASH, pygame.K_v, pygame.K_KP4]
+INVALID_INPUTS = [pg.K_4, pg.K_h, pg.K_i, pg.K_SEMICOLON, pg.K_MINUS, pg.K_LEFTBRACKET, pg.K_QUESTION, pg.K_HASH, pg.K_v, pg.K_KP4]
 
 def loadImage(image):
     filename = os.path.join("images", image)
-    image = pygame.image.load(filename).convert()
+    image = pg.image.load(filename).convert()
     return image, image.get_rect()
     
 #def Terrain():
@@ -21,12 +21,12 @@ def testMove(functionality, Window, Tank):
 	functionality
 	Window.screen.blit(Tank.tankImage, (Tank.tankRect.x,Tank.tankRect.y))
 	Window.screen.blit(Tank.shootyThingImage, (Tank.shootyThingRect.x,Tank.shootyThingRect.y))		
-	pygame.display.update()
+	pg.display.update()
 
 def testAngle(functionality, init_angle, Window, Tank):	
 	angle = init_angle	
 	angle = functionality
 	Window.screen.blit(Tank.tankImage, (Tank.tankRect.x,Tank.tankRect.y))
 	Window.screen.blit(Tank.shootyThingImage, (Tank.shootyThingRect.x,Tank.shootyThingRect.y))
-	pygame.display.update()
+	pg.display.update()
 	return angle
