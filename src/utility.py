2d4c394ba2d5c9c263ddb840a14d23fa7769c02d
clincl <clincl@users.noreply.github.com>
2017-06-26 09:01:15 -0400

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -11,9 +11,9 @@ YELLOW = (255, 255, 51)
 INVALID_INPUTS = [pg.K_4, pg.K_h, pg.K_i, pg.K_SEMICOLON, pg.K_MINUS, pg.K_LEFTBRACKET, pg.K_QUESTION, pg.K_HASH, pg.K_v, pg.K_KP4]
 
 def loadImage(image):
-    filename = os.path.join("images", image)
-    image = pg.image.load(filename).convert()
-    return image, image.get_rect()
+	filename = os.path.join("images", image)
+	image = pg.image.load(filename).convert()
+	return image, image.get_rect()
     
 #def Terrain():
 
