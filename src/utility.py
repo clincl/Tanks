10ea03097d1336a6eab9ec7311113d4180805e29
mcham1 <mcham1@users.noreply.github.com>
2017-06-27 14:12:23 -0400

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -14,6 +14,8 @@ INVALID_INPUTS = [pg.K_4, pg.K_h, pg.K_i, pg.K_SEMICOLON, pg.K_MINUS, pg.K_LEFTB
 TANK_INPUTS = [pg.K_UP, pg.K_DOWN, pg.K_w, pg.K_s, pg.K_SPACE, pg.K_RSHIFT]
 SHOOTYTHING_INPUTS = [pg.K_LEFT, pg.K_RIGHT, pg.K_a, pg.K_d, pg.K_SPACE, pg.K_RSHIFT]
 BULLET_INPUTS = [pg.K_UP, pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT, pg.K_w, pg.K_s, pg.K_a, pg.K_d]
+ANGLE_DICTIONARY = {75:"myShootyThing75.png", 70:"myShootyThing70.png", 65:"myShootyThing65.png", 60:"myShootyThing60.png", 55:"myShootyThing55.png", 50:"myShootyThing50.png", 45:"myShootyThing45.png", 40:"myShootyThing50.png"}
+ANGLE_DICTIONARY_2 = {75:"myShootyThing2_75.png", 70:"myShootyThing2_70.png", 65:"myShootyThing2_65.png", 60:"myShootyThing2_60.png", 55:"myShootyThing2_55.png", 50:"myShootyThing2_50.png", 45:"myShootyThing2_45.png", 40:"myShootyThing2_40.png"}
 
 def loadImage(image):
 	filename = os.path.join("images", image)
@@ -47,11 +49,13 @@ def collisionCheck(MainObject, SpriteGroup):
 		
 def transformShootyThing(Surface, Copy, XDir, YDir, Angle_Dir):
 	Surface = pg.transform.rotozoom(Surface, 2 * Angle_Dir, 1)
-	self.shootyThingRect = self.shootyThingImage.get_rect()
-	Surface.w = Copy.w
-	Surface.h = Copy.h
-	Surface.center = Copy.center
-	print(Surface.width, Surface.height)
-	Surface.x += 1 * XDir
-	Surface.y += 1 * YDir
+	SurfaceRect = Surface.get_rect()
+	print(SurfaceRect.w, SurfaceRect.h)
+	#SurfaceRect.w = Copy.w
+	#SurfaceRect.h = Copy.h
+	Surface.set_clip(Copy)	
+	SurfaceRect.center = Copy.center
+	print(SurfaceRect.width, SurfaceRect.height)
+	SurfaceRect.x += 1 * XDir
+	SurfaceRect.y += 1 * YDir
 	return Surface
