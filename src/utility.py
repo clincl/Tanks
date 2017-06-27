4319930005153cae9f13f403f4a1949151a1d260
mcham1 <mcham1@users.noreply.github.com>
2017-06-27 10:26:59 -0400

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -11,6 +11,9 @@ WHITE = (255, 255, 255)
 PURPLE = (216, 191, 216)
 YELLOW = (255, 255, 51)
 INVALID_INPUTS = [pg.K_4, pg.K_h, pg.K_i, pg.K_SEMICOLON, pg.K_MINUS, pg.K_LEFTBRACKET, pg.K_QUESTION, pg.K_HASH, pg.K_v, pg.K_KP4]
+TANK_INPUTS = [pg.K_UP, pg.K_DOWN, pg.K_w, pg.K_s, pg.K_SPACE, pg.K_RHSIFT]
+SHOOTYTHING_INPUTS = [pg.K_LEFT, pg.K_RIGHT, pg.K_a, pg.K_d, pg.K_SPACE, pg.K_RHSIFT]
+BULLET_INPUTS = [pg.K_UP, pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT, pg.K_w, pg.K_s, pg.K_a, pg.K_d]
 
 def loadImage(image):
 	filename = os.path.join("images", image)
