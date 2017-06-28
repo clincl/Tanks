b2916bc7b3e20038b18d713529f977b366d6555a
niapaulson <npaulso1@binghamton.edu>
2017-06-28 08:50:54 -0400

    Update barrier.py

--- src/barrier.py
+++ src/barrier.py
@@ -1,11 +1,10 @@
-import pygame
+import pygame as pg
 import tank
 
 class Barrier(pg.sprite.Sprite):
 	def __init__(self,x ,y,width =100,height=300 ):
-		pygame.sprite.Sprite.__init__(self)
-		self.barrier = pg.draw.rect(gameDisplay, black, [x,y,width,height])
+		pg.sprite.Sprite.__init__(self)
+		self.barrier = pg.draw.rect(self.screen, black, [x,y,width,height])
 		self.barrierRect.x = x - width/2
         	self.barrierRect.y = y
 		
-main()
