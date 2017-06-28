27e92a6a84b159bf3b89fe5d74d7cda37362a2cd
clincl <clincl@users.noreply.github.com>
2017-06-28 09:19:07 -0400

    Update bullet.py
    parabolic bullet path

--- src/bullet.py
+++ src/bullet.py
@@ -1,20 +1,22 @@
 import pygame as pg
+import math as *
 import utility as u
 
 class Bullet(pg.sprite.Sprite):
-    
-    def __init__(self, x, y, direction, bulletImg):
-        pg.sprite.Sprite.__init__(self)
-        self.bulletImage, self.bulletRect = u.loadImage(bulletImg)
-        self.bulletRect.x = x + 75
-        self.bulletRect.y = y
-        self.bulletXSpeed = 5 * direction
-        self.bulletYSpeed = 5
-        self.bulletMaxY = 150
+
+	def __init__(self, x, y, direction, angle, bulletImg):
+		pg.sprite.Sprite.__init__(self)
+		self.bulletImage, self.bulletRect = u.loadImage(bulletImg)
+		self.bulletRect.x = x + 75
+		self.bulletRect.y = y
+		self.bulletXSpeed = 5 * direction
+		self.bulletYSpeed = 5
+		self.bulletMaxY = 150
+        self.angle = angle
         
-    def update(self):
-        self.bulletRect.x += self.bulletXSpeed
-        self.bulletRect.y += self.bulletYSpeed
-        if self.bulletY == self.bulletMaxY:
-            self.bulletYSpeed *= -1
-        return self.bulletRect.x, self.bulletRect.y
+	def update(self):
+		self.bulletRect.x += self.bulletXSpeed * cos(self.angle)
+		self.bulletRect.y += (self.bulletYSpeed * sin(self.angle)) - u.GRAVITY
+		if self.bulletY == self.bulletMaxY:
+			self.bulletYSpeed *= -1
+		return self.bulletRect.x, self.bulletRect.y
