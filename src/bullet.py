cc383af39c666ffcb7ed8913fc98a551ff5fed7c
clincl <clincl@users.noreply.github.com>
2017-06-28 12:31:05 -0400

    Update bullet.py
    bullet goes up and down and increases and decreases speed.

--- src/bullet.py
+++ src/bullet.py
@@ -11,12 +11,23 @@ class Bullet(pg.sprite.Sprite):
 		self.bulletRect.y = y
 		self.bulletXSpeed = 5 * direction
 		self.bulletYSpeed = 5
-		self.bulletMaxY = 150
+		#self.bulletMaxY = 150
 		self.angle = angle
         
 	def update(self):
 		self.bulletRect.x += self.bulletXSpeed * cos(self.angle)
-		self.bulletRect.y += (self.bulletYSpeed * sin(self.angle)) - u.GRAVITY
-		if self.bulletY == self.bulletMaxY:
-			self.bulletYSpeed *= -1
+		t = 0
+		while self.bulletYSpeed > 0:
+			t +=1
+			self.bulletYSpeed += u.GRAVITY * t #as t increases speed drops
+			self.bulletRect.y += ((self.bulletYSpeed * sin(self.angle)) * t) + ((u.GRAVITY/2) * t**2)
+		if self.bulletYSpeed <= 0:
+			while self.bulletRect.y > u.GROUND:
+				t -= 1
+				self.bulletYSpeed -= u.GRAVITY * t #as t increases speed increases
+				self.bulletRect.y += ((self.bulletYSpeed * sin(self.angle)) * t) + ((u.GRAVITY/2) * t**2)
+
+				
+		#if self.bulletY == self.bulletMaxY:
+		#	self.bulletYSpeed *= -1
 		return self.bulletRect.x, self.bulletRect.y
