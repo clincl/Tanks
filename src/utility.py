86ebb9b7e627a27473b352a7d806982d5978254a
niapaulson <npaulso1@binghamton.edu>
2017-06-28 11:51:26 -0400

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -10,18 +10,24 @@ BLACK = (0,0,0)
 WHITE = (255, 255, 255)
 PURPLE = (216, 191, 216)
 YELLOW = (255, 255, 51)
-GRAVITY = -10
+GRAVITY = 10
 INVALID_INPUTS = [pg.K_4, pg.K_h, pg.K_i, pg.K_SEMICOLON, pg.K_MINUS, pg.K_LEFTBRACKET, pg.K_QUESTION, pg.K_HASH, pg.K_v, pg.K_KP4]
 TANK_INPUTS = [pg.K_UP, pg.K_DOWN, pg.K_w, pg.K_s, pg.K_SPACE, pg.K_RSHIFT]
 SHOOTYTHING_INPUTS = [pg.K_LEFT, pg.K_RIGHT, pg.K_a, pg.K_d, pg.K_SPACE, pg.K_RSHIFT]
 BULLET_INPUTS = [pg.K_UP, pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT, pg.K_w, pg.K_s, pg.K_a, pg.K_d]
-ANGLE_DICTIONARY = {75:"myShootyThing75.png", 70:"myShootyThing70.png", 65:"myShootyThing65.png", 60:"myShootyThing60.png", 55:"myShootyThing55.png", 50:"myShootyThing50.png", 45:"myShootyThing45.png", 40:"myShootyThing50.png"}
+ANGLE_DICTIONARY = {75:"myShootyThing75.png", 70:"myShootyThing70.png", 65:"myShootyThing65.png", 60:"myShootyThing60.png", 55:"myShootyThing55.png", 50:"myShootyThing50.png", 45:"myShootyThing45.png", 40:"myShootyThing40.png"}
 ANGLE_DICTIONARY_2 = {75:"myShootyThing2_75.png", 70:"myShootyThing2_70.png", 65:"myShootyThing2_65.png", 60:"myShootyThing2_60.png", 55:"myShootyThing2_55.png", 50:"myShootyThing2_50.png", 45:"myShootyThing2_45.png", 40:"myShootyThing2_40.png"}
 
 def loadImage(image):
 	filename = os.path.join("images", image)
-	image = pg.image.load(filename).convert()
+	image = pg.image.load(filename).convert_alpha()
 	return image, image.get_rect()
+
+def loadMusic(Music):
+	musicfile = pg.mixer.music.load("music", music)
+	music = pg.mixer.music.play(-1)
+
+
 
 def testMove(iters, direction, Window, Tank):
 	for i in range(iters):
@@ -43,20 +49,12 @@ def testAngle(iters, direction, init_angle, Window, Tank):
 		print("The angle of the shootyThing is ", init_angle)
 	return init_angle
 
-def collisionCheck(MainObject, Group):
-	col = pygame.sprite.collide(MainObject, Group)
+def collisionCheck(MainObject, group):
+	col = pg.sprite.spritecollide(MainObject, group,True)
 	for i in col:
-		Group.remove(i)
+		group.remove(i)
+def groundCheck(group):	
+	for i in group:
+		if i.rect.x < 250:
+			group.remove(i)
 		
-def transformShootyThing(Surface, Copy, XDir, YDir, Angle_Dir):
-	Surface = pg.transform.rotozoom(Surface, 2 * Angle_Dir, 1)
-	SurfaceRect = Surface.get_rect()
-	print(SurfaceRect.w, SurfaceRect.h)
-	#SurfaceRect.w = Copy.w
-	#SurfaceRect.h = Copy.h
-	Surface.set_clip(Copy)	
-	SurfaceRect.center = Copy.center
-	print(SurfaceRect.width, SurfaceRect.height)
-	SurfaceRect.x += 1 * XDir
-	SurfaceRect.y += 1 * YDir
-	return Surface
